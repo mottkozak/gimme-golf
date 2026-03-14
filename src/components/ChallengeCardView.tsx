4f999682e7d561af2fea266d060c8c1b37ed1098
Matt Kozak <matthew.kozak@northwestern.edu>
2026-03-14 08:47:17 -0500

    latest

--- src/components/ChallengeCardView.tsx
+++ src/components/ChallengeCardView.tsx
@@ -99,11 +99,14 @@ function ChallengeCardView({
       <header className="row-between setup-row-wrap">
         <strong>{card.name}</strong>
         <div className="button-row challenge-card__meta">
-          {selected && (
-            <BadgeChip tone="selected" className="challenge-card__selected-chip">
-              ✓ Selected
-            </BadgeChip>
-          )}
+          <BadgeChip
+            tone="selected"
+            className={`challenge-card__selected-chip ${
+              selected ? '' : 'challenge-card__selected-chip--placeholder'
+            }`}
+          >
+            ✓ Selected
+          </BadgeChip>
           <BadgeChip tone="reward" className="challenge-card__points-chip">
             {pointsLabel}
           </BadgeChip>
